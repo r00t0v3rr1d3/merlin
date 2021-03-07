6809e3fb385d9584c7b9db391f1e47671dc59f16
William Tan <williamjefftan@gmail.com>
2021-03-07 17:19:28 -0500

    pthread detach

--- data/bin/so/merlin.c
+++ data/bin/so/merlin.c
@@ -4,6 +4,16 @@
 __attribute__((constructor))
 void init() {
     pthread_t id;
+    pthread_attr_t attr;
+
+    /* Start thread detached */
+    if (-1 == pthread_attr_init(&attr)) {
+            return;
+    }
+    if (-1 == pthread_attr_setdetachstate(&attr,
+                            PTHREAD_CREATE_DETACHED)) {
+            return;
+    }
 
     pthread_create(&id, NULL, Run, NULL);
 }
